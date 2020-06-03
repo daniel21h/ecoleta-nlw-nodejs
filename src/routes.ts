5dd7c8147b6fa9fa31b1df05cb7243051853a293
daniel21h <hesseldaniel21@gmail.com>
2020-06-03 18:09:47 -0300

    update syntax

--- src/routes.ts
+++ src/routes.ts
@@ -31,7 +31,9 @@ routes.post('/points', async(request, response) => {
     items
   } = request.body;
 
-  const ids = await knex('points').insert({
+  const trx = await knex.transaction();
+
+  const insertedIds = await trx('points').insert({
     image: 'image-fake',
     name,
     email,
@@ -41,16 +43,18 @@ routes.post('/points', async(request, response) => {
     city,
     uf
   });
+
+  const point_id = insertedIds[0];
   
   // Relationship with the items table
   const pointItems = items.map((item_id: number) => {
     return {
       item_id,
-      point_id: ids[0],
+      point_id,
     };
   })
 
-  await knex('point_items').insert(pointItems);
+  await trx('point_items').insert(pointItems);
 
   return response.json({ success: true });
 });
