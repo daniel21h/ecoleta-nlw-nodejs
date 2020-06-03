fe96074d0758be02e2845d4b63543b917ee1e744
daniel21h <hesseldaniel21@gmail.com>
2020-06-03 17:38:56 -0300

    feat: create collection point

--- src/routes.ts
+++ src/routes.ts
@@ -3,18 +3,46 @@ import knex from './database/connection';
 
 const routes = express.Router(); 
 
-// GET USERS
+// Get Users
 routes.get('/items', async (request, response) => {
   const items = await knex('items').select('*');
 
   const serializedItems = items.map(item => {
     return {
+      id: item.id,
       name: item.title,
       image_url: `http://localhost:3333/uploads/${item.image}`,
     }
   })
 
   return response.json(serializedItems);
+});
+
+// Create collection point
+routes.post('/points', async(request, response) => {
+  const {
+    name,
+    email,
+    whatsapp,
+    latitude,
+    longitude,
+    city,
+    uf,
+    items
+  } = request.body;
+
+  await knex('points').insert({
+    image: 'image-fake',
+    name,
+    email,
+    whatsapp,
+    latitude,
+    longitude,
+    city,
+    uf
+  });
+
+  return response.json({ success: true });
 });
 
 export default routes;
